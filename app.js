28502a6cf42260c67d8566b01937b7e427409ac9
刘佳黎 <lizi@liujialideMacBook-Pro.local>
2019-02-14 14:35:45 +0800

    123

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ dataAccess.setPoolConfig(require('./conf/mysql'));
 dataAccess.setRedisConfig(require('./conf/redis'));
 const GameConf = require('./app/gameConf/index');
 GameConf.refreshGameConf(success=>{
+    console.log(success);
     if(success){
         Server.startServer(process.argv[3]?process.argv[3]:CommonConf['server_port']);
     }
