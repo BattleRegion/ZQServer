1ba09988e1f6d5673607872a63aa70ff652cd628
刘佳黎 <lizi@liujialideMacBook-Pro.local>
2019-02-21 11:07:30 +0800

    删除 console

--- app/model/game/gconf/global.js
+++ app/model/game/gconf/global.js
@@ -1,8 +1,7 @@
 const conf = require('../../../gameConf/files/global');
-console.log(conf.Global[1]);
+
 module.exports = {
     getGlobalConf: function(key){
-        console.log(conf.Global[1]);
         return conf.Global[1][key];
     },
 };
