cdd423ac4d315e73b8785f449596dc7467fee199
joeyjy <joeyjy@outlook.com>
2019-03-09 23:09:28 +0800

    init player cards when dungeon begins

--- app/model/game/play/card/cardBase.js
+++ app/model/game/play/card/cardBase.js
@@ -1,9 +1,19 @@
 const DataAccess = require('dataAccess');
 const Executor = DataAccess.executor;
 const Command = DataAccess.command;
+const conf = require('../../../gameConf/files/card_base');
+
 module.exports = {
 
-    getCardInfo: function(cardID, cb){
-        
+	Card = conf['Card'],
+
+    getCardInfo: function(cardID){
+        for(let i = 0;i< this.Card.length;i++){
+            let card = this.Card[i];
+            if(card['ID'] === cardID){
+                return card;
+            }
+        }
+        return null;
     }
 };
