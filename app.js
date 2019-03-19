d4ac49afd79c4a518c314fbe3e3c8bf9c0f99705
刘佳黎 <lizi@liujialideiMac.local>
2019-03-19 15:22:44 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 require('./util/global');
 const Server = require('./net/server');
 const GameConf = require('./app/gameConf/index');
-
+const dataAccess = require('dataAccess');
+dataAccess.setPoolConfig(require('./conf/mysql'));
+dataAccess.setRedisConfig(require('./conf/redis'));
 GameConf.refreshGameConf(success=>{
     if(success){
-        const dataAccess = require('dataAccess');
-        dataAccess.setPoolConfig(require('./conf/mysql'));
-        dataAccess.setRedisConfig(require('./conf/redis'));
         Server.startServer(process.argv[3]?process.argv[3]:CommonConf['server_port']);
     }
     else{
