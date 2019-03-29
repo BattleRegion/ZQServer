359bb4ac733084b060bff9429f73a787d0e82b31
guyong <guyong01@chinatopcredit.com>
2019-03-29 23:27:28 +0800

    avatar

---
+++ app/handler/avatar.js
@@ -0,0 +1,69 @@
+const Player = require('../model/game/play/player');
+const AvatarBase = require('../model/game/play/avatar/avatarBase');
+
+module.exports = {
+    //绑定装备到角色
+    // bindAvatar:function(req_p, ws){
+    //     let uid = req_p.rawData.uid;
+    //     let allAvatar = req_p.rawData.allAvatar;
+    //     Player.getPlayerInfo(uid, (e,playerInfo)=>{
+    //         if(e){
+    //             BaseHandler.commonResponse(req_p, {code:e.message},ws);
+    //         }
+    //         else{
+    //             AvatarBase.bind(uid,allAvatar,(e, handInfo)=>{
+    //                 if(e){
+    //                     BaseHandler.commonResponse(req_p, {code:e.message},ws);
+    //                 } else {
+    //                     BaseHandler.commonResponse(req_p, {code:GameCode.SUCCESS, handInfo:handInfo},ws);
+    //                 }
+    //             })
+    //         }
+    //     })
+    // },
+
+    //获取装备信息
+    getAvatarInfo:function(req_p, ws){
+        let avatarId = req_p.rawData.avatarId;
+        return AvatarBase.getAvatarInfo(avatarId)
+    },
+    //获取角色装备
+    getPlayerAvatar:function(req_p, ws){
+        let uid = req_p.rawData.uid;
+        Player.getPlayerInfo(uid, (e,playerInfo)=>{
+            if(e){
+                BaseHandler.commonResponse(req_p, {code:e.message},ws);
+            }
+            else{
+                AvatarBase.getPlayerAvatar(uid,(e, handInfo)=>{
+                    if(e){
+                        BaseHandler.commonResponse(req_p, {code:e.message},ws);
+                    } else {
+                        BaseHandler.commonResponse(req_p, {code:GameCode.SUCCESS, handInfo:handInfo},ws);
+                    }
+                })
+            }
+        })
+    },
+
+    //穿衣
+    equip:function (req_p, ws) {
+        let uid = req_p.rawData.uid;
+        let avatarId = req_p.rawData.avatarId;
+        Player.getPlayerInfo(uid, (e,playerInfo)=>{
+            if(e){
+                BaseHandler.commonResponse(req_p, {code:e.message},ws);
+            }
+            else{
+                AvatarBase.equip(uid,avatarId,(e, handInfo)=>{
+                    if(e){
+                        BaseHandler.commonResponse(req_p, {code:e.message},ws);
+                    } else {
+                        BaseHandler.commonResponse(req_p, {code:GameCode.SUCCESS, handInfo:handInfo},ws);
+                    }
+                })
+            }
+        })
+    }
+
+};
